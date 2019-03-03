6ad3d6090da3e426f12cedbe5baf495015e36306
Suresh Koorella <skoorella@gmail.com>
2019-03-02 20:32:16 -0800

    Added Test Class

---
+++ Projects/test/com/java/prep/JavaLinkedTestTest.java
@@ -0,0 +1,14 @@
+package com.java.prep;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JavaLinkedTestTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
